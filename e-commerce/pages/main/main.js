7e32a6a1ea53757b43695e8fdc404a745bb5f89c
HyperChaos1337 <nikita301002@gmail.com>
2025-04-08 23:58:13 +0300

    about page created

--- e-commerce/pages/main/main.js
+++ e-commerce/pages/main/main.js
@@ -1,14 +1,16 @@
-const menu = document.querySelectorAll('ol > li')
+const menu = document.querySelectorAll('#aside-bar ol > li')
 const pages = document.querySelectorAll('.cards__type')
 
 let currentPage = 1
 
 menu.forEach((element, index) => {
-    element.addEventListener('click', () => currentPage = index + 1)
-    element.addEventListener('click', () => hide())
+    element.addEventListener('click', () => {
+        currentPage = index + 1
+        showPage()
+    })
 })
 
-hide = () => {
+showPage = () => {
     pages.forEach((element, index) => {
         element.style.display = index + 1 === currentPage ? 'grid' : 'none'
     })
